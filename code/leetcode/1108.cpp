554a0d4ffc4c2bec40b5823dbab5dcf07b3ea8c4
wangpengcheng <wangpengcheng2018@gmail.com>
2019-08-24 22:16:04 +0800

    add new

---
+++ code/leetcode/1108.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string defangIPaddr(string& address) {
+        std::string result="";
+            for(int i=0;i<address.length();++i){
+            if(address[i]=='.'){
+                result+="[.]";
+            }else{
+                result+=address[i];
+            }
+        }
+        return result;
+    }
+    string defangIPaddr2(string& address){
+    	char* resutl,temp;
+    	temp=
+    }
+};
+int main(int argc, char const *argv[]) {
+    Solution my_solution;
+    //input string
+    std::string input_string1="1.1.1.1";//目标输出"1[.]1[.]1[.]1"
+    std::string input_string2="255.100.50.0";//目标输出"255[.]100[.]50[.]0"
+    //output string
+    std::string output1="";
+    //my result number
+    auto  my_result1=my_solution.defangIPaddr(input_string1);
+    auto my_result2=my_solution.defangIPaddr(input_string2);
+    //print_vector(my_result);
+    std::cout<<"result1: "<<my_result1<<"\n"<<"resutl2:"<<my_result2<<std::endl;
+
+    //print_vector(my_result);
+    return 0;
+}
