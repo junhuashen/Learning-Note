188819e2488fb30387f524b2991eecc9d6ad0db9
wangpengcheng <wangpengcheng2018@gmail.com>
2019-12-11 15:42:02 +0800

    add new

--- code/leetcode/problems_minimum-window-substring_76.cpp
+++ code/leetcode/problems_minimum-window-substring_76.cpp
@@ -24,9 +24,10 @@ data-time 2019-12-10 20:34:56
 /*
 
 主要思路：1. 使用左右指针分别进行比较运算。
-        时间复杂度O(n^2);空间复杂度O(n);
-        2. 使用回溯的方法。使用数组，保存当前i到j中是否含有t;
-        对于已经
+        时间复杂度O(n^2);空间复杂度O(n); 超时了
+        2. 先让一个指针右移直到找到符合要求的值
+            然后右移指针，如果存在就减小最小值，否则右移左指针
+        时间复杂度O(m+n)
             
 */
 
@@ -80,7 +81,71 @@ public:
         return true;
  
     };
+    string minWindow1(string s, string t) {
+        if(s.empty()||t.empty()) return "";
+        //记录最小长度
+        int min_length=s.size();
+        string result="";
+        if(t.size()==1) {
+        
+            return s.find(t)!=std::string::npos?t:"";
+        }
+        int left=0,right=s.size()-1;
+        if(!isContain(s,t,left,right)){
+            return "0";
+        }
+        while(left<right&&(right-left)>=t.size()){
+            //能够查找到
+            
+            bool is_changed=false;
+            
+            if(isContain(s,t,left+1,right)){
+                ++left;
+                is_changed=true;
+            }
+            if(isContain(s,t,left,right-1)){
+                --right;
+                is_changed=true;
+            }
+            if(!is_changed) break;
+        }
+        result=s.substr(left,right-left+1);
+        return result;
+    }
     string minWindow(string s, string t) {
+        if(s.empty()||t.empty()) return "";
+        //记录最小长度
+        int min_length=t.size();
+        string result="";
+        if(t.size()==1) {
+        
+            return s.find(t)!=std::string::npos?t:"";
+        }
+        int count[256] = { 0 };
+        for (auto c : t) ++count[c];
+        int len = 0, minLength = s.length();
+        for (int l = 0, r = 0; r < s.length(); ++r) {
+            //统计当前字符
+            count[s[r]]--;
+            //检查是否是目标字符串字符
+            if (count[s[r]] >= 0) ++len;
+            //如数目等于t.length就执行一次拷贝
+            while (len == t.length()) {
+                //检查是否小于最小值
+                if (r - l + 1 <= minLength) {
+                    minLength = r - l + 1;
+                    result = s.substr(l, r - l + 1);
+                }
+                //准备开始移动左指针
+                count[s[l]]++;
+                if (count[s[l]] > 0) --len;
+                //移动左指针
+                ++l;
+            }
+        }
+        return result;
+    }
+    string minWindow0(string s, string t) {
         if(s.empty()||t.empty()) return "";
         //记录最小长度
         int min_length=t.size();
@@ -91,7 +156,7 @@ public:
         }
         int left=0,right=s.size()-1;
         if(!isContain(s,t,left,right)){
-            return "0";
+            return "";
         }
     
         for(int i=min_length;i<=s.size();++i){
@@ -115,63 +180,63 @@ int main(int argc, char const *argv[]) {
     vector<int> vector_temp2={2,5,6};
     vector<string >temp={"a"};
 	int time_point_1=clock();
-    auto result=my_solution.minWindow("ADOBECODEBANC","ABC");
+    auto result=my_solution.minWindow("ezsevejszgvxqqggbwkxpwzoyrbaslnxmfdjmmentzllptsspeshatvbkwbcjozwwcfirjxmiixadrsvwmcyfzzpxauhptdlyivrssdadacisxdojhopgogeoalfwoswypnqiqtnqxvkubaeiptpdzvtaizywtuwjhptxkgnhdaceagppbeuabocjpahiudrdskacsqmwqocqurivxcxyqcjfcqqzwheqsfvkxhinvlfrenmcslcinoqsggtcpxtjcowbveosrwjyjvcbigmwueobmjdbgynlojmjpbbjzmhkjjosraomgepsnuvvtkghtttlwwuxjdhsovmfvctdiixxdvtyfzhbuamszipklxezsrgqtavcitzloulvwtwqvklwscgfznguenmzphdxcdlqxwotrkmnxzjrbsxdffxlslkhsohxtupsqdokqaxnzieccdfhjesdpfnktuhoqwgicussurhvalaerfmakgfznslioswerdntxfnuxurzhrfyzrajagkpywypqutjzicxqrtplkqtevtdpuoraagayeppblyavdzluscifsblowqdqeuqectdjkukxumtzogwijenbhapdquuwqmbthgcscmpyaiyorwxaambjntmfnicexfzenbyppoppyngpjdplrtugojmbtqhsvixkjxbylqqmgwbpbtdsozzcinfedpwaxvkhtnhgdxsjtwburephdojodouifqkdowmjjtpmrkwmizjzdygioryrhsznllqbhekqxbeqlcdtbougmcpavptdkuqvfiymmieljkcxnhsovpvjrmjnbcqlwiidsirgqvrcfvbuctlzigicutrxxjlvrvylerrwmkaugbqkxbkhjujdqcdplolejlpndimrtmnzoelnfvupsgukixzwlkaxysmbayuvliubogotdkkxqhhbejxsvxrtpdwsetnrb","ksrsimxsxxjegkkpj");
     int time_point_2=clock();
 	printf("\n \t Time :%d ms \n",time_point_2-time_point_1);
     return 0;
 }
 
 /*
-//优质解析1：思路基本相同，代码更加简洁
+//优质解析1：记录left和right最后再进行拷贝复制
 class Solution {
 public:
-    vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        vector<string> ret;
-        string str;
-        vector<int> index; // 记录每行空格的位置
-        for (int i = 0; i < words.size(); i++) {
-            if (str.length() + words[i].length() < maxWidth) {
-                str += words[i];
-                index.push_back(str.length());
-                str += ' ';
-            } else if (str.length() + words[i].length() == maxWidth) {
-                str += words[i];
-                ret.push_back(str);
-                str.clear();
-                index.clear();
-            } else {
-                int gap = maxWidth - str.length();
-                if (index.size() > 1) {
-                    str.erase(index.back(), 1);
-                    index.pop_back();
-                    gap++;
+    string minWindow(string s, string t) {
+        int count[256] = {0};
+        for(char c : t)
+            count[c]++;
+        int len = 0, l = 0;
+        int min_len = INT_MAX;
+        int min_l = 0;
+
+        for(int r = 0; r < s.length(); ++r){
+            char c1 = s[r];
+            count[c1]--;
+            if(count[c1] >= 0) // 需要字符c1
+                ++len; // 匹配上一个
+            
+            while(len == t.length()){ // 窗口大小大于匹配上字符的个数
+                char c2 = s[l];
+                count[c2]++;
+                if(count[c2] > 0) // 需要字符c1
+                    --len;
+                if(r-l+1 < min_len){
+                    min_l = l;
+                    min_len = r-l+1;
                 }
-                int x = 0, y = 0;
-                if (!index.empty()) {
-                    x = gap / index.size();
-                    y = gap % index.size();
-                }
-                for (int j = index.size()-1; j >= 0; j--) {
-                    str.insert(index[j], string(x + (j < y ? 1 : 0), ' ')); // 从后往前补齐需要的空格
-                }
-                ret.push_back(str);
-                str = words[i];
-                index.clear();
-                if (str.length() < maxWidth) {
-                    index.push_back(str.length());
-                    str += ' ';
-                }
+                l++;
             }
         }
-        if (!str.empty()) {
-            if (str.length() < maxWidth)
-                str += string(maxWidth - str.length(), ' ');
-            ret.push_back(str);
-        }
-        return ret;
+        return min_len == INT_MAX ? "" : s.substr(min_l, min_len);
     }
 };
+//优质解答2:使用map数据结构，减少了运算
 
+class Solution {
+public:
+	string minWindow(string s, string t) {
+        vector<int> map(128,0);
+        for(auto c: t) map[c]++;
+        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
+        while(end<s.size()){
+            if(map[s[end++]]-->0) counter--; //in t
+            while(counter==0){ //valid
+                if(end-begin<d)  d=end-(head=begin);
+                if(map[s[begin++]]++==0) counter++;  //make it invalid
+            }  
+        }
+        return d==INT_MAX? "":s.substr(head, d);
+    }
+};
 
 //优质解析：
 https://leetcode-cn.com/problems/text-justification/solution/hao-shi-0msgu-zhang-by-loli-4/
